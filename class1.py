e4b6483a11fc60fed86736787a11b72ecaa65aba
RT-Jeion <rejuwan002@gmail.com>
2026-02-18 22:15:57 +0600

    Feb 18, 10:15 PM

--- class1.py
+++ class1.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import ast
 # from classes.csv
-class_dt = pd.read_csv("classes.csv")
+class_dt = pd.read_csv("csv_files/classes.csv")
 class_dt = class_dt[["id", "name", "code"]]
 
 # final Data: class_dt.
 
 
 #from scetions.csv
-section_dt = pd.read_csv("sections.csv")
+section_dt = pd.read_csv("csv_files/sections.csv")
 section_dt = section_dt[["id","classes_id" ,"name", "code", "grp_code"]]
 
 # Final data section_dtc
 
 # From classroom.csv
-class_room_dt = pd.read_csv("class_rooms.csv")
+class_room_dt = pd.read_csv("csv_files/class_rooms.csv")
 class_room_dt = class_room_dt[["id", "room_no","number_of_row","number_of_column", "each_brench_capacity"]]
 class_room_dt["total_capacity"] = class_room_dt["number_of_row"] * class_room_dt["number_of_column"] * class_room_dt["each_brench_capacity"]
 class_room_dt = class_room_dt.sort_values("room_no")
@@ -23,20 +23,20 @@ class_room_dt = class_room_dt.sort_values("room_no")
 
 
 #from shifts_management_logs.csv
-shift_dt = pd.read_csv("shift_management_logs.csv")
+shift_dt = pd.read_csv("csv_files/shift_management_logs.csv")
 shift_dt = shift_dt[["id","weekends", "start", "end"]]
 
 # Final shift_dt
 
 
 # from subjects.csv
-sub_dt = pd.read_csv("subjects.csv")
+sub_dt = pd.read_csv("csv_files/subjects.csv")
 sub_dt = sub_dt[["id", "name", "code", "department"]]
 # Final sub_dt
 
 
 # from subject_groups.csv
-sub_grp_dt = pd.read_csv("subject_groups.csv")
+sub_grp_dt = pd.read_csv("csv_files/subject_groups.csv")
 sub_grp_dt = sub_grp_dt[["id", "name", "grp_code", "has_subjects"]]
 sub_grp_dt = sub_grp_dt.rename(columns={"id":"grp_id"})
 sub_grp_dt["has_subjects"] = sub_grp_dt["has_subjects"].apply(ast.literal_eval)
@@ -44,7 +44,7 @@ sub_grp_dt["has_subjects"] = sub_grp_dt["has_subjects"].apply(ast.literal_eval)
 
 
 # From teachers.csv
-teacher_dt = pd.read_csv("teachers.csv")
+teacher_dt = pd.read_csv("csv_files/teachers.csv")
 teacher_dt = teacher_dt[["id","name","code", "department", "designation"]]
 
 # Final teacher_dt
