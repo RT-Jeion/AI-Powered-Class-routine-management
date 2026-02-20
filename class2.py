9e3c90457a9e0f9c10c39d0c83d9d7102d61e548
RT-Jeion <rejuwan002@gmail.com>
2026-02-20 06:31:06 +0600

    Feb 20, 06:40AM

--- class2.py
+++ class2.py
@@ -4,13 +4,12 @@ from main import teacher_dt, sec_sub_dt, sub_dt
 def find_eligible(subject_ids):
     dept_list = sub_dt[sub_dt["id"].isin(subject_ids)]["department"].unique()
     eligible = teacher_dt[teacher_dt["department"].isin(dept_list)]
-    return list(eligible["name"])
+    return list(eligible["code"])
 
 
 sec_teacher_dt = sec_sub_dt[["code", "grp_code"]].copy()
 sec_teacher_dt["teachers"] = sec_sub_dt["has_subjects"].apply(find_eligible)
 
-print(sec_teacher_dt)
+
 if __name__ == "__main__":
-    print(sec_sub_dt)
-    print(teacher_dt)
+    print(sec_teacher_dt)
